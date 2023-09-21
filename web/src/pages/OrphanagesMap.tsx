a7c7e0b4eb640bebc610e77f444927f47d3ac3a3
marcelonfilho <marcelonfilho.ms@gmail.com>
2023-09-21 02:38:57 -0400

    update project

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi'
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import 'dotenv/config'
 
 import mapMarkerImg from '../images/map-marker.svg';
 import '../styles/pages/orphanages-map.css'
